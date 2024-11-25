f7711b0fc9f72f571e5062c96040aeb871a82ca8
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:23:49 +0000

    Revert "[Commit] Iteration 160 #errors = 2537 <= 2536."
    This reverts commit 5a97ded457f65cd4884dd6e6ac411fa679417e8e.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Extensions/EmailMessageExtensionsTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Extensions/EmailMessageExtensionsTests.cs
@@ -9,7 +9,6 @@ using NUnit.Framework;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.Email;
 using Umbraco.Cms.Infrastructure.Extensions;
-using MimeKit;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Extensions
 {
@@ -17,27 +16,6 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Extensions
     public class EmailMessageExtensionsTests
     {
         private const string ConfiguredSender = "noreply@umbraco.com";
-
-        private static MimeMessage ConvertToMimeMessage(EmailMessage emailMessage, string configuredSender)
-        {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress(emailMessage.From ?? configuredSender));
-            message.To.Add(new MailboxAddress(emailMessage.To));
-            message.Subject = emailMessage.Subject;
-
-            var builder = new BodyBuilder();
-            if (emailMessage.IsBodyHtml)
-            {
-                builder.HtmlBody = emailMessage.Body;
-            }
-            else
-            {
-                builder.TextBody = emailMessage.Body;
-            }
-
-            message.Body = builder.ToMessageBody();
-            return message;
-        }
 
         [Test]
         public void Can_Construct_MimeMessage_From_Simple_EmailMessage()
@@ -49,7 +27,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Extensions
             const bool isBodyHtml = true;
             var emailMessage = new EmailMessage(from, to, subject, body, isBodyHtml);
 
-            var result = ConvertToMimeMessage(emailMessage, ConfiguredSender);
+            var result = emailMessage.ToMimeMessage(ConfiguredSender);
 
             Assert.AreEqual(1, result.From.Count());
             Assert.AreEqual(from, result.From.First().ToString());
@@ -57,7 +35,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Extensions
             Assert.AreEqual(to, result.To.First().ToString());
             Assert.AreEqual(subject, result.Subject);
             Assert.IsNull(result.TextBody);
-            Assert.AreEqual(body, result.HtmlBody);
+            Assert.AreEqual(body, result.HtmlBody.ToString());
         }
 
         [Test]
