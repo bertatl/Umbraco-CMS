5edd3e27f5a32d21be9cb76058f8110f4c7ce3fc
NoReply Amazon <no-reply@amazon.com>
2024-11-25 16:42:34 +0000

    [Commit] Iteration 212 #errors = 2640 <= 2642.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs][CS0122][23,59] 'BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<TOptions>' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs
@@ -2,7 +2,12 @@
 // See LICENSE for more details.
 
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using NUnit.Framework;
+using System;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Web.BackOffice.Security;
 
@@ -20,10 +25,26 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.BackOffice.Security
                 SignInScheme = "my_cookie"
             };
 
-            var sut = new BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<RemoteAuthenticationOptions>();
-            sut.PostConfigure(scheme, options);
+            var builder = new BackOfficeAuthenticationBuilder(null);
+            builder.AddScheme<RemoteAuthenticationOptions, TestAuthHandler>(scheme, options =>
+            {
+                options.SignInScheme = "my_cookie";
+            });
 
-            Assert.AreEqual(options.SignInScheme, Constants.Security.BackOfficeExternalAuthenticationType);
+            Assert.AreEqual(Constants.Security.BackOfficeExternalAuthenticationType, options.SignInScheme);
+        }
+
+        private class TestAuthHandler : AuthenticationHandler<RemoteAuthenticationOptions>
+        {
+            public TestAuthHandler(IOptionsMonitor<RemoteAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+                : base(options, logger, encoder, clock)
+            {
+            }
+
+            protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+            {
+                throw new NotImplementedException();
+            }
         }
 
         [Test]
