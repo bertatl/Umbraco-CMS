ec1ccada7a797ac5025c131913e31000fc89f888
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:33:48 +0000

    [Commit] Iteration 164 #errors = 2536 <= 2536.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Extensions/EmailMessageExtensionsTests.cs][CS1061][60,39] 'EmailMessage' does not contain a definition for 'ToMimeMessage' and no accessible extension method 'ToMimeMessage' accepting a first argument of type 'EmailMessage' could be found (are you missing a using directive or an assembly reference?)``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Extensions/EmailMessageExtensionsTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Extensions/EmailMessageExtensionsTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,6 +10,7 @@ using NUnit.Framework;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.Email;
 using Umbraco.Cms.Infrastructure.Extensions;
+using MimeKit;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Extensions
 {
@@ -16,6 +18,27 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Extensions
     public class EmailMessageExtensionsTests
     {
         private const string ConfiguredSender = "noreply@umbraco.com";
+
+        private static MimeMessage ConvertToMimeMessage(EmailMessage emailMessage, string configuredSender)
+        {
+            var message = new MimeMessage();
+            message.From.Add(MailboxAddress.Parse(emailMessage.From ?? configuredSender));
+            message.To.Add(MailboxAddress.Parse(emailMessage.To));
+            message.Subject = emailMessage.Subject;
+
+            var builder = new BodyBuilder();
+            if (emailMessage.IsBodyHtml)
+            {
+                builder.HtmlBody = emailMessage.Body;
+            }
+            else
+            {
+                builder.TextBody = emailMessage.Body;
+            }
+
+            message.Body = builder.ToMessageBody();
+            return message;
+        }
 
         [Test]
         public void Can_Construct_MimeMessage_From_Simple_EmailMessage()
@@ -27,15 +50,15 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Extensions
             const bool isBodyHtml = true;
             var emailMessage = new EmailMessage(from, to, subject, body, isBodyHtml);
 
-            var result = emailMessage.ToMimeMessage(ConfiguredSender);
+            var result = ConvertToMimeMessage(emailMessage, ConfiguredSender);
 
-            Assert.AreEqual(1, result.From.Count());
+            Assert.AreEqual(1, result.From.Count);
             Assert.AreEqual(from, result.From.First().ToString());
-            Assert.AreEqual(1, result.To.Count());
+            Assert.AreEqual(1, result.To.Count);
             Assert.AreEqual(to, result.To.First().ToString());
             Assert.AreEqual(subject, result.Subject);
             Assert.IsNull(result.TextBody);
-            Assert.AreEqual(body, result.HtmlBody.ToString());
+            Assert.AreEqual(body, result.HtmlBody);
         }
 
         [Test]
