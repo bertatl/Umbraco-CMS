8fe9d31e9d7698d0242681086848ec4e45f489fc
NoReply Amazon <no-reply@amazon.com>
2024-11-25 22:02:06 +0000

    Revert "[Commit] Iteration 123 #errors = 2592 <= 2586."
    This reverts commit a57b52488cf729998a60926c6320e232258191a7.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Logging/LogviewerTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Logging/LogviewerTests.cs
@@ -250,24 +250,10 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Logging
         }
 
         private IList<ILogViewerQuery> Store { get; }
-        private LogViewerQueryModelFactory LogViewerQueryModelFactory { get; } = new LogViewerQueryModelFactory();
+        private LogViewerQueryRepository.LogViewerQueryModelFactory LogViewerQueryModelFactory { get; } = new LogViewerQueryRepository.LogViewerQueryModelFactory();
+
 
         public ILogViewerQuery Get(int id) => Store.FirstOrDefault(x => x.Id == id);
-
-        private class LogViewerQueryModelFactory
-        {
-            public ILogViewerQuery BuildEntity(string name, string query)
-            {
-                return new LogViewerQuery { Name = name, Query = query };
-            }
-        }
-
-        private class LogViewerQuery : ILogViewerQuery
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Query { get; set; }
-        }
 
         public IEnumerable<ILogViewerQuery> GetMany(params int[] ids) =>
             ids.Any() ? Store.Where(x => ids.Contains(x.Id)) : Store;
