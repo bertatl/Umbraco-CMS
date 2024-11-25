419b66af8cb2d062b65f43771d0573fc776706f6
NoReply Amazon <no-reply@amazon.com>
2024-11-25 20:37:37 +0000

    [Commit] Iteration 337 #errors = 2462 <= 2463.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs][CS0122][103,26] 'FullDataSetRepositoryCachePolicy<TEntity, TId>' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
@@ -100,7 +100,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
             Assert.IsNotNull(list);
 
             // Do it again, ensure that its coming from the cache!
-            policy = new FullDataSetRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id, false);
+            policy = new DefaultRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, GetDefaultOptions());
 
             found = policy.GetAll(new object[] { }, ids => getAll);
 
