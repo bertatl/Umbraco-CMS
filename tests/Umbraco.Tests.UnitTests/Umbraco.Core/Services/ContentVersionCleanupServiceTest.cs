ed8a83edaadc4ca4130e23d8b5155cc627802cc3
NoReply Amazon <no-reply@amazon.com>
2024-11-25 19:15:00 +0000

    [Commit] Iteration 41 #errors = 2748 <= 2748.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/ContentVersionCleanupServiceTest.cs][CS0122][29,13] 'ContentVersionService' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/ContentVersionCleanupServiceTest.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/ContentVersionCleanupServiceTest.cs
@@ -26,7 +26,7 @@ namespace Umbraco.Tests.Services
             [Frozen] Mock<IDocumentVersionRepository> documentVersionRepository,
             List<ContentVersionMeta> someHistoricVersions,
             DateTime aDateTime,
-            ContentVersionService sut)
+            Umbraco.Cms.Core.Services.Implement.ContentVersionService sut)
         {
             documentVersionRepository.Setup(x => x.GetDocumentVersionsEligibleForCleanup())
                 .Returns(someHistoricVersions);
