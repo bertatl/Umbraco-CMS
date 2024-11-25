c9ebafa475768816d70889b4bfdda84295815644
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:37:18 +0000

    Revert "[Commit] Iteration 428 #errors = 2108 <= 2108."
    This reverts commit a486c3c95436893dffaaf33b3aa9786d0fc0994e.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/ImageCropperTest.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/ImageCropperTest.cs
@@ -13,7 +13,6 @@ using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Media;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
-using Umbraco.Cms.Core.Strings;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common
