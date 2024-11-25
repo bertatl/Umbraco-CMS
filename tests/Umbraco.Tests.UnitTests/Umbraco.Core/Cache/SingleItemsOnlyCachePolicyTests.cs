c6ecb7e6b172a4d21c9f5384b8f7fb91827ff903
NoReply Amazon <no-reply@amazon.com>
2024-11-25 22:49:25 +0000

    [Commit] Iteration 146 #errors = 2561 <= 2561.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/SingleItemsOnlyCachePolicyTests.cs][CS1662][40,80] Cannot convert lambda expression to intended delegate type because some of the return types in the block are not implicitly convertible to the delegate return type``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/SingleItemsOnlyCachePolicyTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/SingleItemsOnlyCachePolicyTests.cs
@@ -12,39 +12,39 @@ using Umbraco.Cms.Core.Scoping;
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
 {
     [TestFixture]
-    public class SingleItemsOnlyCachePolicyTests
+public class SingleItemsOnlyCachePolicyTests
+{
+    private IScopeAccessor DefaultAccessor
     {
-        private IScopeAccessor DefaultAccessor
+        get
         {
-            get
-            {
-                var accessor = new Mock<IScopeAccessor>();
-                var scope = new Mock<IScope>();
-                scope.Setup(x => x.RepositoryCacheMode).Returns(RepositoryCacheMode.Default);
-                accessor.Setup(x => x.AmbientScope).Returns(scope.Object);
-                return accessor.Object;
-            }
+            var accessor = new Mock<IScopeAccessor>();
+            var scope = new Mock<IScope>();
+            scope.Setup(x => x.RepositoryCacheMode).Returns(RepositoryCacheMode.Default);
+            accessor.Setup(x => x.AmbientScope).Returns(scope.Object);
+            return accessor.Object;
         }
+    }
 
-        [Test]
-        public void Get_All_Doesnt_Cache()
-        {
-            var cached = new List<string>();
-            var cache = new Mock<IAppPolicyCache>();
-            cache.Setup(x => x.Insert(It.IsAny<string>(), It.IsAny<Func<object>>(), It.IsAny<TimeSpan?>(), It.IsAny<bool>(), It.IsAny<string[]>()))
-                .Callback((string cacheKey, Func<object> o, TimeSpan? t, bool b, string[] s) => cached.Add(cacheKey));
-            cache.Setup(x => x.SearchByKey(It.IsAny<string>())).Returns(new AuditItem[] { });
+    [Test]
+    public void Get_All_Doesnt_Cache()
+    {
+        var cached = new List<string>();
+        var cache = new Mock<IAppPolicyCache>();
+        cache.Setup(x => x.Insert(It.IsAny<string>(), It.IsAny<Func<object>>(), It.IsAny<TimeSpan?>(), It.IsAny<bool>(), It.IsAny<string[]>()))
+            .Callback((string cacheKey, Func<object> o, TimeSpan? t, bool b, string[] s) => cached.Add(cacheKey));
+        cache.Setup(x => x.SearchByKey(It.IsAny<string>())).Returns(new AuditItem[] { });
 
-            var defaultPolicy = new SingleItemsOnlyRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, new RepositoryCachePolicyOptions());
+        var defaultPolicy = new SingleItemsOnlyRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, new RepositoryCachePolicyOptions());
 
-            AuditItem[] unused = defaultPolicy.GetAll(new object[] { }, ids => new[]
-                    {
-                        new AuditItem(1, AuditType.Copy, 123, "test", "blah"),
-                        new AuditItem(2, AuditType.Copy, 123, "test", "blah2")
-                    });
+        IEnumerable<AuditItem> unused = defaultPolicy.GetAll(new object[] { }, ids => new[]
+                {
+                    new AuditItem(1, AuditType.Copy, 123, "test", "blah"),
+                    new AuditItem(2, AuditType.Copy, 123, "test", "blah2")
+                });
 
-            Assert.AreEqual(0, cached.Count);
-        }
+        Assert.AreEqual(0, cached.Count);
+    }
 
         [Test]
         public void Caches_Single()
