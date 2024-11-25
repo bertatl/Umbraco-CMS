8ea62b1d03b642a334d5266a088b41c412cce9f5
NoReply Amazon <no-reply@amazon.com>
2024-11-25 14:21:07 +0000

    Revert "[Commit] Iteration 143 #errors = 2736 <= 2735."
    This reverts commit 43b198a139ca2e70127ed5234e37fdbe6132e71d.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Scoping/ScopeUnitTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Scoping/ScopeUnitTests.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Umbraco.Cms.Core.Scoping;
-using Umbraco.Cms.Infrastructure.Scoping;
 using CSharpTest.Net.Interfaces;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
