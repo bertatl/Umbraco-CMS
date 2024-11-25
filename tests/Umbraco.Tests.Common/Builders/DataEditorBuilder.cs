2dbc09a8b26288357f93bd3344621795ab7390b2
NoReply Amazon <no-reply@amazon.com>
2024-11-25 18:46:18 +0000

    [Commit] Iteration 27 #errors = 2750 <= 2749.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.Common/Builders/DataEditorBuilder.cs][CS1729][58,24] 'DataEditor' does not contain a constructor that takes 6 arguments``.

--- tests/Umbraco.Tests.Common/Builders/DataEditorBuilder.cs
+++ tests/Umbraco.Tests.Common/Builders/DataEditorBuilder.cs
@@ -8,6 +8,7 @@ using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Cms.Core.Serialization;
 using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Cms.Tests.Common.Builders.Extensions;
 using Umbraco.Cms.Tests.Common.Builders.Interfaces;
 using Moq;
@@ -55,13 +56,15 @@ namespace Umbraco.Cms.Tests.Common.Builders
 
             var ioHelper = new Mock<IIOHelper>().Object;
             var jsonSerializer = new Mock<IJsonSerializer>().Object;
+            var propertyEditorCollection = Mock.Of<IPropertyEditorCollection>();
             return new DataEditor(
+                ioHelper,
+                jsonSerializer,
+                propertyEditorCollection,
                 name,
                 alias,
                 explicitValueEditor,
-                explicitConfigurationEditor,
-                ioHelper,
-                jsonSerializer)
+                explicitConfigurationEditor)
             {
                 DefaultConfiguration = defaultConfiguration
             };
