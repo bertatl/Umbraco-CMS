347d96aa58e71f3f5113ac8ae988fef2ab41c758
NoReply Amazon <no-reply@amazon.com>
2024-11-25 22:14:58 +0000

    [Commit] Iteration 385 #errors = 2202 <= 2375.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Persistence/BulkDataReaderTests.cs][CS1674][80,20] 'BulkDataReaderTests.BulkDataReaderSubclass': type used in a using statement must be implicitly convertible to 'System.IDisposable'.``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Persistence/BulkDataReaderTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Persistence/BulkDataReaderTests.cs
@@ -23,17 +23,41 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Persistence
     public class BulkDataReaderTests
     {
         // Add this line to make BulkDataReader accessible for testing
-public class BulkDataReader
+public abstract class BulkDataReader : IDataReader
 {
-    public virtual object GetValue(int i)
-    {
-        throw new NotImplementedException();
-    }
-
-    public virtual bool Read()
-    {
-        throw new NotImplementedException();
-    }
+    public virtual object GetValue(int i) => throw new NotImplementedException();
+    public virtual bool Read() => throw new NotImplementedException();
+    public virtual void Close() => throw new NotImplementedException();
+    public virtual bool NextResult() => throw new NotImplementedException();
+    public virtual int Depth => throw new NotImplementedException();
+    public virtual bool IsClosed => throw new NotImplementedException();
+    public virtual int RecordsAffected => throw new NotImplementedException();
+    public virtual int FieldCount => throw new NotImplementedException();
+    public virtual void Dispose() => throw new NotImplementedException();
+    public virtual bool GetBoolean(int i) => throw new NotImplementedException();
+    public virtual byte GetByte(int i) => throw new NotImplementedException();
+    public virtual long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length) => throw new NotImplementedException();
+    public virtual char GetChar(int i) => throw new NotImplementedException();
+    public virtual long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length) => throw new NotImplementedException();
+    public virtual IDataReader GetData(int i) => throw new NotImplementedException();
+    public virtual string GetDataTypeName(int i) => throw new NotImplementedException();
+    public virtual DateTime GetDateTime(int i) => throw new NotImplementedException();
+    public virtual decimal GetDecimal(int i) => throw new NotImplementedException();
+    public virtual double GetDouble(int i) => throw new NotImplementedException();
+    public virtual Type GetFieldType(int i) => throw new NotImplementedException();
+    public virtual float GetFloat(int i) => throw new NotImplementedException();
+    public virtual Guid GetGuid(int i) => throw new NotImplementedException();
+    public virtual short GetInt16(int i) => throw new NotImplementedException();
+    public virtual int GetInt32(int i) => throw new NotImplementedException();
+    public virtual long GetInt64(int i) => throw new NotImplementedException();
+    public virtual string GetName(int i) => throw new NotImplementedException();
+    public virtual int GetOrdinal(string name) => throw new NotImplementedException();
+    public virtual string GetString(int i) => throw new NotImplementedException();
+    public virtual int GetValues(object[] values) => throw new NotImplementedException();
+    public virtual bool IsDBNull(int i) => throw new NotImplementedException();
+    public virtual object this[int i] => throw new NotImplementedException();
+    public virtual object this[string name] => throw new NotImplementedException();
+    public virtual DataTable GetSchemaTable() => throw new NotImplementedException();
 }
         /// <summary>
         /// The <see cref="BulkDataReaderSubclass"/> schema name.
