3efcf5ea087a61de63745a70df581923372864e2
NoReply Amazon <no-reply@amazon.com>
2024-11-25 22:04:15 +0000

    [Commit] Iteration 124 #errors = 2584 <= 2586.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Logging/LogviewerTests.cs][CS0122][253,17] 'LogViewerQueryRepository' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Logging/LogviewerTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Logging/LogviewerTests.cs
@@ -246,14 +246,26 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Logging
         public TestLogViewerQueryRepository()
         {
             Store = new List<ILogViewerQuery>(DefaultLogQueries
-                .Select(x => LogViewerQueryModelFactory.BuildEntity(x.Name, x.Query)));
+                .Select(x => new LogViewerQuery { Name = x.Name, Query = x.Query }));
         }
 
         private IList<ILogViewerQuery> Store { get; }
-        private LogViewerQueryRepository.LogViewerQueryModelFactory LogViewerQueryModelFactory { get; } = new LogViewerQueryRepository.LogViewerQueryModelFactory();
-
 
         public ILogViewerQuery Get(int id) => Store.FirstOrDefault(x => x.Id == id);
+
+        private class LogViewerQuery : ILogViewerQuery
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Query { get; set; }
+            public object Key => Id;
+            public DateTime CreateDate { get; set; }
+            public DateTime UpdateDate { get; set; }
+            public DateTime? DeleteDate { get; set; }
+            public bool HasIdentity => Id > 0;
+            public void ResetIdentity() => Id = default;
+            public object DeepClone() => MemberwiseClone();
+        }
 
         public IEnumerable<ILogViewerQuery> GetMany(params int[] ids) =>
             ids.Any() ? Store.Where(x => ids.Contains(x.Id)) : Store;
