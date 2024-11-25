e6f9c5686142248723d0d5316307cff05b9c5410
NoReply Amazon <no-reply@amazon.com>
2024-11-25 16:03:42 +0000

    Revert "[Commit] Iteration 194 #errors = 2684 <= 2683."
    This reverts commit f59e2d5f79c2d8e5a2ad13469b5a4ebc8a8dc3d5.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
@@ -1,10 +1,9 @@
-// Copyright (c) Umbraco.
+﻿// Copyright (c) Umbraco.
 // See LICENSE for more details.
 
 using NUnit.Framework;
 using Umbraco.Cms.Infrastructure.WebAssets;
 using Umbraco.Extensions;
-using Moq;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
 {
@@ -14,11 +13,10 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
         [Test]
         public void Parse_Main()
         {
-            // Create a mock for BackOfficeJavaScriptInitializer
-            var mockInitializer = new Mock<IBackOfficeJavaScriptInitializer>();
+            var result = BackOfficeJavaScriptInitializer.WriteScript("[World]", "Hello", "Blah");
 
-            // Set up the mock to return the expected result
-            string expectedResult = @"LazyLoad.js([World], function () {
+            Assert.AreEqual(
+                @"LazyLoad.js([World], function () {
     //we need to set the legacy UmbClientMgr path
     if ((typeof UmbClientMgr) !== ""undefined"") {
         UmbClientMgr.setUmbracoPath('Hello');
@@ -29,13 +27,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
         angular.bootstrap(document, ['Blah']);
 
     });
-});";
-            mockInitializer.Setup(m => m.WriteScript("[World]", "Hello", "Blah")).Returns(expectedResult);
-
-            // Use the mock object to get the result
-            var result = mockInitializer.Object.WriteScript("[World]", "Hello", "Blah");
-
-            Assert.AreEqual(expectedResult.StripWhitespace(), result.StripWhitespace());
+});".StripWhitespace(), result.StripWhitespace());
         }
     }
 }
