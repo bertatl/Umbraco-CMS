abeedac0fc9f7a5ccb6e168b79bbcacddbf71585
NoReply Amazon <no-reply@amazon.com>
2024-11-25 20:54:49 +0000

    [Commit] Iteration 346 #errors = 2423 <= 2422.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Core/Components/ComponentTests.cs][CS0122][183,33] 'ComposerGraph' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Components/ComponentTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Components/ComponentTests.cs
@@ -30,6 +30,7 @@ using Umbraco.Cms.Infrastructure.Persistence.Mappers;
 using Umbraco.Cms.Tests.UnitTests.TestHelpers;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Composers;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Components
 {
