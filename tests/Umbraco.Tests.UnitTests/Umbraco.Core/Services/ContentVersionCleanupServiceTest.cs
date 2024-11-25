3be4247bb4aa2c835aa192f008e1e4da94e4d52b
NoReply Amazon <no-reply@amazon.com>
2024-11-25 19:17:07 +0000

    Revert "[Commit] Iteration 42 #errors = 2749 <= 2748."
    This reverts commit 561e01dcf3a3d78b458711f502ff7a247ac511d2.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/ContentVersionCleanupServiceTest.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/ContentVersionCleanupServiceTest.cs
@@ -18,8 +18,6 @@ namespace Umbraco.Tests.Services
     [TestFixture]
     internal class ContentVersionCleanupServiceTest
     {
-        // Ensure ContentVersionService is accessible
-        private ContentVersionService _contentVersionService;
         [Test]
         [AutoMoqData]
         public void PerformContentVersionCleanup_Always_RespectsDeleteRevisionsCancellation(
@@ -28,7 +26,7 @@ namespace Umbraco.Tests.Services
             [Frozen] Mock<IDocumentVersionRepository> documentVersionRepository,
             List<ContentVersionMeta> someHistoricVersions,
             DateTime aDateTime,
-            [Frozen] ContentVersionService sut)
+            ContentVersionService sut)
         {
             documentVersionRepository.Setup(x => x.GetDocumentVersionsEligibleForCleanup())
                 .Returns(someHistoricVersions);
