b2ad8d6247402fe319e3ce9eefe1b4b1ccdd7481
NoReply Amazon <no-reply@amazon.com>
2024-11-25 14:54:38 +0000

    Revert "[Commit] Iteration 159 #errors = 2726 <= 2732."
    This reverts commit 6919b84431557869705b564ead220c24897a87a5.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/UserDataServiceTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/UserDataServiceTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -120,14 +120,14 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Services
             Assert.AreEqual(isDebug.ToString(), actual.Data);
         }
 
-        private IUserDataProvider CreateUserDataService(string culture = "", ModelsMode modelsMode = ModelsMode.InMemoryAuto, bool isDebug = true)
+        private SystemInformationTelemetryProvider CreateUserDataService(string culture = "", ModelsMode modelsMode = ModelsMode.InMemoryAuto, bool isDebug = true)
         {
             var localizationService = CreateILocalizationService(culture);
 
             var databaseMock = new Mock<IUmbracoDatabase>();
             databaseMock.Setup(x => x.DatabaseType.GetProviderName()).Returns("SQL");
 
-            var provider = new SystemInformationTelemetryProvider(
+            return new SystemInformationTelemetryProvider(
                 _umbracoVersion,
                 localizationService,
                 Mock.Of<IOptions<ModelsBuilderSettings>>(x => x.Value == new ModelsBuilderSettings { ModelsMode = modelsMode }),
@@ -135,8 +135,6 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Services
                 Mock.Of<IOptions<GlobalSettings>>(x => x.Value == new GlobalSettings()),
                 Mock.Of<IHostEnvironment>(),
                 new Lazy<IUmbracoDatabase>(databaseMock.Object));
-
-            return provider;
         }
 
         private ILocalizationService CreateILocalizationService(string culture)
