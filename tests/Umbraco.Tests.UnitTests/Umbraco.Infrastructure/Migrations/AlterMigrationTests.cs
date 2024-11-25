9e64b169af6c7babbe8af7ce72aea50e00bdc1ce
NoReply Amazon <no-reply@amazon.com>
2024-11-25 14:01:53 +0000

    [Commit] Iteration 134 #errors = 2736 <= 2739.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Migrations/AlterMigrationTests.cs][CS0122][27,17] 'MigrationContext' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Migrations/AlterMigrationTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Migrations/AlterMigrationTests.cs
@@ -17,14 +17,14 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Migrations
     [TestFixture]
     public class AlterMigrationTests
     {
-        private readonly ILogger<MigrationContext> _logger = Mock.Of<ILogger<MigrationContext>>();        
+        private readonly ILogger<IMigrationContext> _logger = Mock.Of<ILogger<IMigrationContext>>();
         private class TestPlan : MigrationPlan
         {
             public TestPlan() : base("Test")
             {
             }
         }
-        private MigrationContext GetMigrationContext(out TestDatabase db)
+        private IMigrationContext GetMigrationContext(out TestDatabase db)
         {
             db = new TestDatabase();
             return new MigrationContext(new TestPlan(), db, _logger);
