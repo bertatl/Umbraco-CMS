a486c3c95436893dffaaf33b3aa9786d0fc0994e
NoReply Amazon <no-reply@amazon.com>
2024-11-25 23:37:18 +0000

    [Commit] Iteration 428 #errors = 2108 <= 2108.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/ImageCropperTest.cs][CS1061][42,59] 'string' does not contain a definition for 'DeserializeImageCropperValue' and no accessible extension method 'DeserializeImageCropperValue' accepting a first argument of type 'string' could be found (are you missing a using directive or an assembly reference?)``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/ImageCropperTest.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/ImageCropperTest.cs
@@ -13,6 +13,7 @@ using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Media;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
+using Umbraco.Cms.Core.Strings;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common
