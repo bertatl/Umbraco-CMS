d8e75b545899237717940c80414506203ca9d134
NoReply Amazon <no-reply@amazon.com>
2024-11-25 17:54:38 +0000

    [Commit] Iteration 2 #errors = 2853 <= 11531.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/src/Umbraco.Infrastructure/Persistence/NPocoDatabaseExtensions-Bulk.cs][CS0103][35,13] The name 'SqlBulkCopyHelper' does not exist in the current context``.

--- src/Umbraco.Infrastructure/Persistence/NPocoDatabaseExtensions-Bulk.cs
+++ src/Umbraco.Infrastructure/Persistence/NPocoDatabaseExtensions-Bulk.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using NPoco;
+using NPoco.SqlServer;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Infrastructure.Persistence;
 
