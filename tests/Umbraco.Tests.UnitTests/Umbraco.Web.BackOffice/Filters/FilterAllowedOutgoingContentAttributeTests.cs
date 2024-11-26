4e8484e67abd33f96e5b3b3c89193c1ddde4eb38
NoReply Amazon <no-reply@amazon.com>
2024-11-26 23:40:10 +0000

    [Commit] Iteration 475 #errors = 2419 <= 2419.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Filters/FilterAllowedOutgoingContentAttributeTests.cs][CS0122][139,17] 'FilterAllowedOutgoingMediaFilter.FilterBasedOnStartNode(IList, IUser)' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Filters/FilterAllowedOutgoingContentAttributeTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Filters/FilterAllowedOutgoingContentAttributeTests.cs
@@ -20,6 +20,7 @@ using Umbraco.Cms.Web.Common.Filters;
 using Umbraco.Extensions;
 using Umbraco.Cms.Web.BackOffice.Filters;
 using System;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.BackOffice.Filters
 {
@@ -136,10 +137,23 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.BackOffice.Filters
                 list.Add(new ContentItemBasic { Id = i, Name = "Test" + i, ParentId = i, Path = path });
             }
 
-            att.FilterBasedOnStartNode(list, user);
+            // Create a mock ActionExecutingContext
+            var actionContext = new ActionContext();
+            var actionExecutingContext = new ActionExecutingContext(
+                actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object>(),
+                new Mock<Controller>().Object);
+            actionExecutingContext.Result = new ObjectResult(list);
 
-            Assert.AreEqual(5, list.Count);
-       }
+            // Execute the filter
+            att.OnActionExecuting(actionExecutingContext);
+
+            // Check the filtered result
+            var filteredList = ((ObjectResult)actionExecutingContext.Result).Value as List<ContentItemBasic>;
+            Assert.IsNotNull(filteredList);
+            Assert.AreEqual(5, filteredList.Count);
+        }
 
         [Test]
         public void Filter_On_Permissions()
