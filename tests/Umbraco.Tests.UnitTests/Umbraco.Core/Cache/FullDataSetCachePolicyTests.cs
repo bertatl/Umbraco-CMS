fe9280fc524d43740c72b992e7945b61a6d85590
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:33:59 +0000

    Revert "[Commit] Iteration 192 #errors = 2518 <= 2516."
    This reverts commit c9a81268fac92382267d050991f2c84001bb40b7.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
@@ -12,7 +12,6 @@ using Umbraco.Cms.Core.Collections;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Infrastructure.Persistence.Repositories;
-using Umbraco.Cms.Infrastructure.Persistence.Repositories.Implement;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
 {
@@ -99,9 +98,9 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
             Assert.IsNotNull(list);
 
             // Do it again, ensure that its coming from the cache!
-            var fullDataSetPolicy = new FullDataSetRepositoryCachePolicy<AuditItem, int>(cache.Object, DefaultAccessor, item => item.Id, false);
+            policy = new FullDataSetRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id, false);
 
-            found = fullDataSetPolicy.GetAll(new int[] { }, ids => getAll.AsEnumerable());
+            found = policy.GetAll(new object[] { }, ids => getAll);
 
             Assert.AreEqual(1, cached.Count);
             Assert.IsNotNull(list);
