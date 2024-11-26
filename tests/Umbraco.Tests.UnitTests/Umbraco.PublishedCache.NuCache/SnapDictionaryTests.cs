33ffdf8b6ed9cc4d4a5e00ad0da2541b6aff8369
NoReply Amazon <no-reply@amazon.com>
2024-11-26 04:25:16 +0000

    Revert "[Commit] Iteration 304 #errors = 2468 <= 2468."
    This reverts commit 4b7ca49537c81191cc13a99679a48cd9ca826914.

--- tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs
@@ -101,7 +101,7 @@ public class TestHelper
             // gen 1
             d.Set(1, "one");
             Assert.AreEqual(1, d.Count);
-            d.TryRemove(1, out _);
+            d.Clear(1);
             Assert.AreEqual(0, d.Count); // gone
 
             // We can't assert on internal state, so we'll remove these assertions
