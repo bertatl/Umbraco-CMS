c4d181b0a9aec081e4a6a37b7b16c7a38c7044ae
NoReply Amazon <no-reply@amazon.com>
2024-11-26 03:33:23 +0000

    Revert "[Commit] Iteration 273 #errors = 2423 <= 2429."
    This reverts commit 797498302d9a101305c4e9ec67d487bc8ffa8f4e.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/Filters/ValidateUmbracoFormRouteStringFilterTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/Filters/ValidateUmbracoFormRouteStringFilterTests.cs
@@ -2,14 +2,7 @@
 // See LICENSE for more details.
 
 using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Abstractions;
-using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Routing;
-using Moq;
 using NUnit.Framework;
-using System.Collections.Generic;
 using Umbraco.Cms.Web.Common.Exceptions;
 using Umbraco.Cms.Web.Common.Filters;
 using Umbraco.Cms.Web.Common.Security;
@@ -19,78 +12,32 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.Filters
     [TestFixture]
     public class ValidateUmbracoFormRouteStringFilterTests
     {
-        private Mock<IDataProtectionProvider> _mockDataProtectionProvider;
-        private ValidateUmbracoFormRouteStringAttribute _attribute;
-
-        [SetUp]
-        public void SetUp()
-        {
-            _mockDataProtectionProvider = new Mock<IDataProtectionProvider>();
-            _attribute = new ValidateUmbracoFormRouteStringAttribute();
-            _attribute.DataProtectionProvider = _mockDataProtectionProvider.Object;
-        }
+        private IDataProtectionProvider DataProtectionProvider { get; } = new EphemeralDataProtectionProvider();
 
         [Test]
         public void Validate_Route_String()
         {
+            var filter = new ValidateUmbracoFormRouteStringAttribute.ValidateUmbracoFormRouteStringFilter(DataProtectionProvider);
+
+            Assert.Throws<HttpUmbracoFormRouteStringException>(() => filter.ValidateRouteString(null, null, null, null));
+
             const string ControllerName = "Test";
             const string ControllerAction = "Index";
             const string Area = "MyArea";
+            var validUfprt = EncryptionHelper.CreateEncryptedRouteString(DataProtectionProvider, ControllerName, ControllerAction, Area);
 
-            var mockDataProtector = new Mock<IDataProtector>();
-            _mockDataProtectionProvider.Setup(x => x.CreateProtector(It.IsAny<string>())).Returns(mockDataProtector.Object);
+            var invalidUfprt = validUfprt + "z";
+            Assert.Throws<HttpUmbracoFormRouteStringException>(() => filter.ValidateRouteString(invalidUfprt, null, null, null));
 
-            var validUfprt = "validEncryptedString";
-            mockDataProtector.Setup(x => x.Unprotect(It.Is<byte[]>(b => System.Text.Encoding.UTF8.GetString(b) == validUfprt)))
-                .Returns(System.Text.Encoding.UTF8.GetBytes($"{ControllerName}|{ControllerAction}|{Area}"));
+            Assert.Throws<HttpUmbracoFormRouteStringException>(() => filter.ValidateRouteString(validUfprt, ControllerName, ControllerAction, "doesntMatch"));
+            Assert.Throws<HttpUmbracoFormRouteStringException>(() => filter.ValidateRouteString(validUfprt, ControllerName, ControllerAction, null));
+            Assert.Throws<HttpUmbracoFormRouteStringException>(() => filter.ValidateRouteString(validUfprt, ControllerName, "doesntMatch", Area));
+            Assert.Throws<HttpUmbracoFormRouteStringException>(() => filter.ValidateRouteString(validUfprt, ControllerName, null, Area));
+            Assert.Throws<HttpUmbracoFormRouteStringException>(() => filter.ValidateRouteString(validUfprt, "doesntMatch", ControllerAction, Area));
+            Assert.Throws<HttpUmbracoFormRouteStringException>(() => filter.ValidateRouteString(validUfprt, null, ControllerAction, Area));
 
-            var context = CreateActionExecutingContext(ControllerName, ControllerAction, Area);
-
-            // Test valid case
-            context.HttpContext.Request.Form = new FormCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>
-            {
-                { "ufprt", validUfprt }
-            });
-            Assert.DoesNotThrow(() => _attribute.OnActionExecuting(context));
-
-            // Test invalid ufprt
-            context.HttpContext.Request.Form = new FormCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>
-            {
-                { "ufprt", "invalidUfprt" }
-            });
-            Assert.Throws<HttpUmbracoFormRouteStringException>(() => _attribute.OnActionExecuting(context));
-
-            // Test missing ufprt
-            context.HttpContext.Request.Form = new FormCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>());
-            Assert.Throws<HttpUmbracoFormRouteStringException>(() => _attribute.OnActionExecuting(context));
-
-            // Test mismatched area
-            context.RouteData.Values["area"] = "WrongArea";
-            context.HttpContext.Request.Form = new FormCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>
-            {
-                { "ufprt", validUfprt }
-            });
-            Assert.Throws<HttpUmbracoFormRouteStringException>(() => _attribute.OnActionExecuting(context));
-        }
-
-        private ActionExecutingContext CreateActionExecutingContext(string controller, string action, string area)
-        {
-            var httpContext = new DefaultHttpContext();
-            var actionContext = new ActionContext(
-                httpContext,
-                new RouteData(new RouteValueDictionary
-                {
-                    { "controller", controller },
-                    { "action", action },
-                    { "area", area }
-                }),
-                new ActionDescriptor());
-
-            return new ActionExecutingContext(
-                actionContext,
-                new List<IFilterMetadata>(),
-                new Dictionary<string, object>(),
-                new Mock<Controller>().Object);
+            Assert.DoesNotThrow(() => filter.ValidateRouteString(validUfprt, ControllerName, ControllerAction, Area));
+            Assert.DoesNotThrow(() => filter.ValidateRouteString(validUfprt, ControllerName.ToLowerInvariant(), ControllerAction.ToLowerInvariant(), Area.ToLowerInvariant()));
         }
     }
 }
