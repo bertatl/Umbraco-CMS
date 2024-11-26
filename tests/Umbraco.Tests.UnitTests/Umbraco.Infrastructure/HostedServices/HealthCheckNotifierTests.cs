058175f026ba63b698a36a0bcc3b3c91168b43bc
NoReply Amazon <no-reply@amazon.com>
2024-11-26 22:15:33 +0000

    Revert "[Commit] Iteration 183 #errors = 2464 <= 2463."
    This reverts commit 50f7d3ac560312aad9b813554a778ae50a3c6900.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/HostedServices/HealthCheckNotifierTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/HostedServices/HealthCheckNotifierTests.cs
@@ -99,11 +99,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.HostedServices
             await sut.PerformExecuteAsync(null);
             _mockNotificationMethod.Verify(
                 x => x.SendAsync(
-                    It.Is<HealthCheckResults>(y =>
-                        y.ResultsAsDictionary.Count == 1 &&
-                        y.ResultsAsDictionary.ContainsKey("Check1") &&
-                        y.ResultsAsDictionary["Check1"].Any())),
-                Times.Once);
+                    It.Is<HealthCheckResults>(y => y.ResultsAsDictionary.Count == 1 && y.ResultsAsDictionary.ContainsKey("Check1"))), Times.Once);
         }
 
         private HealthCheckNotifier CreateHealthCheckNotifier(
