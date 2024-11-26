9f6f0156a300d8100e5b5bbfce76edb10f996dda
NoReply Amazon <no-reply@amazon.com>
2024-11-26 19:58:48 +0000

    Revert "[Commit] Iteration 108 #errors = 2608 <= 2608."
    This reverts commit 4d9314503691ef59f3620cc770a1ca6dcc73d4bb.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
@@ -12,7 +12,6 @@ using Umbraco.Cms.Core.Collections;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Core.Cache;
-using Umbraco.Cms.Infrastructure.Persistence.Repositories;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
 {
