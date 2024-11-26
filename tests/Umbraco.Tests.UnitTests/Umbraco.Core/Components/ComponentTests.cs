6a33cb827329c2be974608aad9fcaf6c5abe0f73
NoReply Amazon <no-reply@amazon.com>
2024-11-26 01:31:20 +0000

    [Commit] Iteration 217 #errors = 2496 <= 2497.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Core/Components/ComponentTests.cs][CS0122][96,33] 'ComposerGraph' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Components/ComponentTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Components/ComponentTests.cs
@@ -29,6 +29,7 @@ using Umbraco.Cms.Infrastructure.Migrations.Install;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Infrastructure.Persistence.Mappers;
 using Umbraco.Cms.Tests.UnitTests.TestHelpers;
+using Umbraco.Cms.Core.Composing;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Components
 {
@@ -93,13 +94,13 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Components
             var composition = new UmbracoBuilder(register, Mock.Of<IConfiguration>(), TestHelper.GetMockedTypeLoader());
 
             Type[] types = TypeArray<Composer1, Composer2, Composer4>();
-            var composers = new ComposerGraph(composition, types, Enumerable.Empty<Attribute>(), Mock.Of<ILogger<ComposerGraph>>());
+            var componentLoader = new ComponentLoader(Mock.Of<ILogger<ComponentLoader>>(), composition);
             Composed.Clear();
 
             // 2 is Core and requires 4
             // 3 is User
             // => reorder components accordingly
-            composers.Compose();
+            componentLoader.Compose(types);
             AssertTypeArray(TypeArray<Composer1, Composer4, Composer2>(), Composed);
 
             IServiceProvider factory = MockFactory(m =>
