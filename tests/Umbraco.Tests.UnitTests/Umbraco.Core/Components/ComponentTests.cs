1c8d99fdb5fde3657bd7231cdbaf27b59fa3fc35
NoReply Amazon <no-reply@amazon.com>
2024-11-26 01:31:20 +0000

    Revert "[Commit] Iteration 217 #errors = 2496 <= 2497."
    This reverts commit 6a33cb827329c2be974608aad9fcaf6c5abe0f73.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Components/ComponentTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Components/ComponentTests.cs
@@ -29,7 +29,6 @@ using Umbraco.Cms.Infrastructure.Migrations.Install;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Infrastructure.Persistence.Mappers;
 using Umbraco.Cms.Tests.UnitTests.TestHelpers;
-using Umbraco.Cms.Core.Composing;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Components
 {
@@ -94,13 +93,13 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Components
             var composition = new UmbracoBuilder(register, Mock.Of<IConfiguration>(), TestHelper.GetMockedTypeLoader());
 
             Type[] types = TypeArray<Composer1, Composer2, Composer4>();
-            var componentLoader = new ComponentLoader(Mock.Of<ILogger<ComponentLoader>>(), composition);
+            var composers = new ComposerGraph(composition, types, Enumerable.Empty<Attribute>(), Mock.Of<ILogger<ComposerGraph>>());
             Composed.Clear();
 
             // 2 is Core and requires 4
             // 3 is User
             // => reorder components accordingly
-            componentLoader.Compose(types);
+            composers.Compose();
             AssertTypeArray(TypeArray<Composer1, Composer4, Composer2>(), Composed);
 
             IServiceProvider factory = MockFactory(m =>
