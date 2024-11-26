7a1c45965dcf9e95bf5053f44c88f2bc3e79bb8e
NoReply Amazon <no-reply@amazon.com>
2024-11-26 04:31:16 +0000

    [Commit] Iteration 307 #errors = 2468 <= 2468.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs][CS1061][104,15] 'SnapDictionary<int, string>' does not contain a definition for 'Set' and no accessible extension method 'Set' accepting a first argument of type 'SnapDictionary<int, string>' could be found (are you missing a using directive or an assembly reference?)``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs
@@ -99,9 +99,9 @@ public class TestHelper
             Assert.AreEqual(0, d.Count);
 
             // gen 1
-            d.Set(1, "one");
+            d[1] = "one";
             Assert.AreEqual(1, d.Count);
-            d.Set(1, null); // Set to null to effectively remove the item
+            d[1] = null; // Set to null to effectively remove the item
             Assert.AreEqual(0, d.Count); // gone
 
             // We can't assert on internal state, so we'll remove these assertions
