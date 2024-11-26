d2b1feb1d51e982144bfb245cde7e4466a877d6b
NoReply Amazon <no-reply@amazon.com>
2024-11-26 21:06:26 +0000

    Revert "[Commit] Iteration 144 #errors = 2630 <= 2629."
    This reverts commit 62abf78e3245d72da25d47f8d2c812f24821f902.

--- tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs
@@ -60,7 +60,7 @@ public static class SnapDictionaryTestHelperExtensions
     public static void SetCollectAuto<TKey, TValue>(this SnapDictionary<TKey, TValue> dictionary, bool value)
         where TValue : class
     {
-        var testHelper = (SnapDictionary<TKey, TValue>.TestHelper)dictionary.GetTestHelper();
+        var testHelper = dictionary.GetTestHelper();
         testHelper.CollectAuto = value;
     }
 }
