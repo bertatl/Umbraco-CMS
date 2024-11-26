d54353bc0dc008769e264da82bca2aec6fff466e
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:04:33 +0000

    [Commit] Iteration 176 #errors = 2532 <= 2529.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs][CS0122][16,58] 'BackOfficeJavaScriptInitializer.WriteScript(string, string, string)' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
@@ -1,19 +1,32 @@
-﻿// Copyright (c) Umbraco.
+// Copyright (c) Umbraco.
 // See LICENSE for more details.
 
 using NUnit.Framework;
+using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Infrastructure.WebAssets;
 using Umbraco.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
 {
     [TestFixture]
     public class JsInitializationTests
     {
+        private IBackOfficeJavaScriptInitializer _initializer;
+
+        [SetUp]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IBackOfficeJavaScriptInitializer, BackOfficeJavaScriptInitializer>();
+            var serviceProvider = services.BuildServiceProvider();
+            _initializer = serviceProvider.GetRequiredService<IBackOfficeJavaScriptInitializer>();
+        }
+
         [Test]
         public void Parse_Main()
         {
-            var result = BackOfficeJavaScriptInitializer.WriteScript("[World]", "Hello", "Blah");
+            var result = _initializer.Initialize("[World]", "Hello", "Blah");
 
             Assert.AreEqual(
                 @"LazyLoad.js([World], function () {
