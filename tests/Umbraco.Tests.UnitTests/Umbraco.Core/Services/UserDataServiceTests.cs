28380d89a6238b815c68a38c1437c46125ceccaa
NoReply Amazon <no-reply@amazon.com>
2024-11-26 18:23:03 +0000

    [Commit] Iteration 55 #errors = 2694 <= 2694.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/UserDataServiceTests.cs][CS0122][123,17] 'SystemInformationTelemetryProvider' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/UserDataServiceTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/UserDataServiceTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -14,6 +14,7 @@ using Umbraco.Cms.Core.Semver;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Infrastructure.Telemetry.Providers;
+using Umbraco.Cms.Core.Telemetry;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Services
 {
