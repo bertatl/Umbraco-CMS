8aad3d2fad05f65e35e6ddb1b50f999b579b2e87
NoReply Amazon <no-reply@amazon.com>
2024-11-26 22:57:23 +0000

    [Commit] Iteration 205 #errors = 2449 <= 2444.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Filters/FilterAllowedOutgoingContentAttributeTests.cs][CS0246][32,27] The type or namespace name 'FilterAllowedOutgoingContentFilter' could not be found (are you missing a using directive or an assembly reference?)``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Filters/FilterAllowedOutgoingContentAttributeTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Filters/FilterAllowedOutgoingContentAttributeTests.cs
@@ -18,6 +18,7 @@ using Umbraco.Cms.Tests.Common.Builders;
 using Umbraco.Cms.Tests.Common.Builders.Extensions;
 using Umbraco.Cms.Web.Common.Filters;
 using Umbraco.Extensions;
+using Umbraco.Cms.Web.BackOffice.Filters;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.BackOffice.Filters
 {
