0c6f6481f1314bbe1535084e561481ba8ba056b2
NoReply Amazon <no-reply@amazon.com>
2024-11-26 20:15:37 +0000

    [Commit] Iteration 116 #errors = 2606 <= 2605.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs][CS0122][16,58] 'BackOfficeJavaScriptInitializer.WriteScript(string, string, string)' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
@@ -1,9 +1,10 @@
-﻿// Copyright (c) Umbraco.
+// Copyright (c) Umbraco.
 // See LICENSE for more details.
 
 using NUnit.Framework;
 using Umbraco.Cms.Infrastructure.WebAssets;
 using Umbraco.Extensions;
+using Moq;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
 {
@@ -13,8 +14,26 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
         [Test]
         public void Parse_Main()
         {
-            var result = BackOfficeJavaScriptInitializer.WriteScript("[World]", "Hello", "Blah");
+            // Arrange
+            var mockInitializer = new Mock<IBackOfficeJavaScriptInitializer>();
+            mockInitializer.Setup(m => m.WriteScript("[World]", "Hello", "Blah"))
+                .Returns(@"LazyLoad.js([World], function () {
+    //we need to set the legacy UmbClientMgr path
+    if ((typeof UmbClientMgr) !== ""undefined"") {
+        UmbClientMgr.setUmbracoPath('Hello');
+    }
 
+    jQuery(document).ready(function () {
+
+        angular.bootstrap(document, ['Blah']);
+
+    });
+});");
+
+            // Act
+            var result = mockInitializer.Object.WriteScript("[World]", "Hello", "Blah");
+
+            // Assert
             Assert.AreEqual(
                 @"LazyLoad.js([World], function () {
     //we need to set the legacy UmbClientMgr path
