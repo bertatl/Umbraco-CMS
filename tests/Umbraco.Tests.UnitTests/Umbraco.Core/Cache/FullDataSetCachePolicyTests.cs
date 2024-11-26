93dd841b1495283e226961d07b5b49cbf6743acd
NoReply Amazon <no-reply@amazon.com>
2024-11-26 21:33:01 +0000

    Revert "[Commit] Iteration 159 #errors = 2487 <= 2494."
    This reverts commit 80170761af18cf92c76ef87f2732f880fd3367c4.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
@@ -12,12 +12,11 @@ using Umbraco.Cms.Core.Collections;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Infrastructure.Persistence.Repositories;
-using Umbraco.Cms.Infrastructure.Persistence.Caching;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
 {
     [TestFixture]
-    public class DefaultRepositoryCachePolicyTests
+    public class FullDataSetCachePolicyTests
     {
         private IScopeAccessor DefaultAccessor
         {
@@ -66,7 +65,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
             var cache = new Mock<IAppPolicyCache>();
             cache.Setup(x => x.Get(It.IsAny<string>())).Returns(new AuditItem(1, AuditType.Copy, 123, "test", "blah"));
 
-            var defaultPolicy = new DefaultRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id);
+            var defaultPolicy = new FullDataSetRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id, false);
 
             AuditItem found = defaultPolicy.Get(1, id => null, ids => getAll);
             Assert.IsNotNull(found);
@@ -94,7 +93,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
             cache.Setup(x => x.Get(It.IsAny<string>()))
                 .Returns(() => cached.Any() ? new DeepCloneableList<AuditItem>(ListCloneBehavior.CloneOnce) : null);
 
-            IRepositoryCachePolicy<AuditItem, object> policy = new DefaultRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id);
+            IRepositoryCachePolicy<AuditItem, object> policy = new FullDataSetRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id, false);
 
             AuditItem[] found = policy.GetAll(new object[] { }, ids => getAll);
 
@@ -132,7 +131,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
                 });
             cache.Setup(x => x.Get(It.IsAny<string>())).Returns(new AuditItem[] { });
 
-            var defaultPolicy = new DefaultRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id);
+            var defaultPolicy = new FullDataSetRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id, false);
 
             AuditItem[] found = defaultPolicy.GetAll(new object[] { }, ids => getAll);
 
@@ -153,7 +152,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
                 new AuditItem(2, AuditType.Copy, 123, "test", "blah2")
             });
 
-            var defaultPolicy = new DefaultRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id);
+            var defaultPolicy = new FullDataSetRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id, false);
 
             AuditItem[] found = defaultPolicy.GetAll(new object[] { }, ids => getAll);
             Assert.AreEqual(2, found.Length);
@@ -173,7 +172,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
             cache.Setup(x => x.Clear(It.IsAny<string>()))
                 .Callback(() => cacheCleared = true);
 
-            var defaultPolicy = new DefaultRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id);
+            var defaultPolicy = new FullDataSetRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id, false);
             try
             {
                 defaultPolicy.Update(new AuditItem(1, AuditType.Copy, 123, "test", "blah"), item => throw new Exception("blah!"));
@@ -202,7 +201,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
             cache.Setup(x => x.Clear(It.IsAny<string>()))
                 .Callback(() => cacheCleared = true);
 
-            var defaultPolicy = new DefaultRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id);
+            var defaultPolicy = new FullDataSetRepositoryCachePolicy<AuditItem, object>(cache.Object, DefaultAccessor, item => item.Id, false);
             try
             {
                 defaultPolicy.Delete(new AuditItem(1, AuditType.Copy, 123, "test", "blah"), item => throw new Exception("blah!"));
