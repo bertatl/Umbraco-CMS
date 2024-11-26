ce7ae642da514112fecea6d690bb515e2dd4bd15
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:51:41 +0000

    [Commit] Iteration 198 #errors = 2510 <= 2510.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs][CS0122][23,59] 'BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<TOptions>' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs
@@ -2,6 +2,7 @@
 // See LICENSE for more details.
 
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Web.BackOffice.Security;
@@ -14,31 +15,41 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.BackOffice.Security
         [Test]
         public void EnsureBackOfficeScheme_When_Backoffice_Auth_Scheme_Expect_Updated_SignInScheme()
         {
+            // Arrange
+            var services = new ServiceCollection();
+            var authBuilder = new AuthenticationBuilder(services);
+            var backOfficeAuthBuilder = new BackOfficeAuthenticationBuilder(authBuilder);
             var scheme = $"{Constants.Security.BackOfficeExternalAuthenticationTypePrefix}test";
             var options = new RemoteAuthenticationOptions
             {
                 SignInScheme = "my_cookie"
             };
 
-            var sut = new BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<RemoteAuthenticationOptions>();
-            sut.PostConfigure(scheme, options);
+            // Act
+            backOfficeAuthBuilder.ConfigureBackOfficeAuthenticationOptions(scheme, options);
 
-            Assert.AreEqual(options.SignInScheme, Constants.Security.BackOfficeExternalAuthenticationType);
+            // Assert
+            Assert.AreEqual(Constants.Security.BackOfficeExternalAuthenticationType, options.SignInScheme);
         }
 
         [Test]
         public void EnsureBackOfficeScheme_When_Not_Backoffice_Auth_Scheme_Expect_No_Change()
         {
+            // Arrange
+            var services = new ServiceCollection();
+            var authBuilder = new AuthenticationBuilder(services);
+            var backOfficeAuthBuilder = new BackOfficeAuthenticationBuilder(authBuilder);
             var scheme = "test";
             var options = new RemoteAuthenticationOptions
             {
                 SignInScheme = "my_cookie"
             };
 
-            var sut = new BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<RemoteAuthenticationOptions>();
-            sut.PostConfigure(scheme, options);
+            // Act
+            backOfficeAuthBuilder.ConfigureBackOfficeAuthenticationOptions(scheme, options);
 
-            Assert.AreNotEqual(options.SignInScheme, Constants.Security.BackOfficeExternalAuthenticationType);
+            // Assert
+            Assert.AreEqual("my_cookie", options.SignInScheme);
         }
     }
 }
