e8f13b1576a8837afaac031bf8b44d419b893e27
NoReply Amazon <no-reply@amazon.com>
2024-11-26 02:10:51 +0000

    Revert "[Commit] Iteration 236 #errors = 2489 <= 2485."
    This reverts commit d2409cf4b99dece22fbfe87e01d5a24c2fd477bb.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs
@@ -2,14 +2,10 @@
 // See LICENSE for more details.
 
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Moq;
 using NUnit.Framework;
 using Umbraco.Cms.Core;
-using Umbraco.Cms.Core.Configuration.Models;
-using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Web.BackOffice.Security;
+using Microsoft.Extensions.Options;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.BackOffice.Security
 {
@@ -17,63 +13,33 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.BackOffice.Security
     public class BackOfficeAuthenticationBuilderTests
     {
         [Test]
-        public void AddBackOfficeExternalLogins_When_Backoffice_Auth_Scheme_Expect_Updated_SignInScheme()
+        public void EnsureBackOfficeScheme_When_Backoffice_Auth_Scheme_Expect_Updated_SignInScheme()
         {
-            // Arrange
-            var services = new ServiceCollection();
-            var mockRuntimeState = new Mock<IRuntimeState>();
-            mockRuntimeState.Setup(x => x.Level).Returns(RuntimeLevel.Run);
-
-            var globalSettings = new GlobalSettings();
-
             var scheme = $"{Constants.Security.BackOfficeExternalAuthenticationTypePrefix}test";
-            services.AddAuthentication()
-                .AddRemoteScheme<RemoteAuthenticationOptions, RemoteAuthenticationHandler>(scheme, scheme, _ => { });
+            var options = new RemoteAuthenticationOptions
+            {
+                SignInScheme = "my_cookie"
+            };
 
-            var builder = new BackOfficeAuthenticationBuilder(services, mockRuntimeState.Object, Options.Create(globalSettings));
-
-            // Act
-            builder.AddBackOfficeExternalLogins();
-
-            // Assert
-            var provider = services.BuildServiceProvider();
-            var schemeProvider = provider.GetRequiredService<IAuthenticationSchemeProvider>();
-            var remoteScheme = schemeProvider.GetSchemeAsync(scheme).GetAwaiter().GetResult();
-            var options = provider.GetRequiredService<IOptionsMonitor<RemoteAuthenticationOptions>>().Get(scheme);
+            var sut = new BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<RemoteAuthenticationOptions>();
+            ((IPostConfigureOptions<RemoteAuthenticationOptions>)sut).PostConfigure(scheme, options);
 
             Assert.AreEqual(Constants.Security.BackOfficeExternalAuthenticationType, options.SignInScheme);
         }
 
         [Test]
-        public void AddBackOfficeExternalLogins_When_Not_Backoffice_Auth_Scheme_Expect_No_Change()
+        public void EnsureBackOfficeScheme_When_Not_Backoffice_Auth_Scheme_Expect_No_Change()
         {
-            // Arrange
-            var services = new ServiceCollection();
-            var mockRuntimeState = new Mock<IRuntimeState>();
-            mockRuntimeState.Setup(x => x.Level).Returns(RuntimeLevel.Run);
-
-            var globalSettings = new GlobalSettings();
-
             var scheme = "test";
-            var originalSignInScheme = "my_cookie";
-            services.AddAuthentication()
-                .AddRemoteScheme<RemoteAuthenticationOptions, RemoteAuthenticationHandler>(scheme, scheme, options =>
-                {
-                    options.SignInScheme = originalSignInScheme;
-                });
+            var options = new RemoteAuthenticationOptions
+            {
+                SignInScheme = "my_cookie"
+            };
 
-            var builder = new BackOfficeAuthenticationBuilder(services, mockRuntimeState.Object, Options.Create(globalSettings));
+            var sut = new BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<RemoteAuthenticationOptions>();
+            ((IPostConfigureOptions<RemoteAuthenticationOptions>)sut).PostConfigure(scheme, options);
 
-            // Act
-            builder.AddBackOfficeExternalLogins();
-
-            // Assert
-            var provider = services.BuildServiceProvider();
-            var schemeProvider = provider.GetRequiredService<IAuthenticationSchemeProvider>();
-            var remoteScheme = schemeProvider.GetSchemeAsync(scheme).GetAwaiter().GetResult();
-            var options = provider.GetRequiredService<IOptionsMonitor<RemoteAuthenticationOptions>>().Get(scheme);
-
-            Assert.AreEqual(originalSignInScheme, options.SignInScheme);
+            Assert.AreEqual("my_cookie", options.SignInScheme);
         }
     }
 }
