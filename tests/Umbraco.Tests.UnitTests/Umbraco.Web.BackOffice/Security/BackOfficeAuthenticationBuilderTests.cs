65827208425c2ed65020af8ac81b9f53c3a7eb95
NoReply Amazon <no-reply@amazon.com>
2024-11-26 18:57:20 +0000

    Revert "[Commit] Iteration 75 #errors = 2664 <= 2664."
    This reverts commit f8341f8f590cc5dba1422e8a264922b21f0c96c8.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.BackOffice/Security/BackOfficeAuthenticationBuilderTests.cs
@@ -2,7 +2,6 @@
 // See LICENSE for more details.
 
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Web.BackOffice.Security;
@@ -13,47 +12,33 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.BackOffice.Security
     public class BackOfficeAuthenticationBuilderTests
     {
         [Test]
-        public void AddBackOfficeExternalLogins_When_Backoffice_Auth_Scheme_Expect_Updated_SignInScheme()
+        public void EnsureBackOfficeScheme_When_Backoffice_Auth_Scheme_Expect_Updated_SignInScheme()
         {
-            var services = new ServiceCollection();
-            var builder = new BackOfficeAuthenticationBuilder(services);
-
             var scheme = $"{Constants.Security.BackOfficeExternalAuthenticationTypePrefix}test";
-            builder.AddBackOfficeExternalLogins(logins =>
+            var options = new RemoteAuthenticationOptions
             {
-                logins.AddBackOfficeExternalLogin<RemoteAuthenticationOptions>(
-                    scheme,
-                    "Test Provider",
-                    options => options.SignInScheme = "my_cookie");
-            });
+                SignInScheme = "my_cookie"
+            };
 
-            var authOptions = services.BuildServiceProvider()
-                .GetRequiredService<IOptionsMonitor<RemoteAuthenticationOptions>>()
-                .Get(scheme);
+            var sut = new BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<RemoteAuthenticationOptions>();
+            sut.PostConfigure(scheme, options);
 
-            Assert.AreEqual(Constants.Security.BackOfficeExternalAuthenticationType, authOptions.SignInScheme);
+            Assert.AreEqual(options.SignInScheme, Constants.Security.BackOfficeExternalAuthenticationType);
         }
 
         [Test]
-        public void AddBackOfficeExternalLogins_When_Not_Backoffice_Auth_Scheme_Expect_No_Change()
+        public void EnsureBackOfficeScheme_When_Not_Backoffice_Auth_Scheme_Expect_No_Change()
         {
-            var services = new ServiceCollection();
-            var builder = new BackOfficeAuthenticationBuilder(services);
-
             var scheme = "test";
-            builder.AddBackOfficeExternalLogins(logins =>
+            var options = new RemoteAuthenticationOptions
             {
-                logins.AddBackOfficeExternalLogin<RemoteAuthenticationOptions>(
-                    scheme,
-                    "Test Provider",
-                    options => options.SignInScheme = "my_cookie");
-            });
+                SignInScheme = "my_cookie"
+            };
 
-            var authOptions = services.BuildServiceProvider()
-                .GetRequiredService<IOptionsMonitor<RemoteAuthenticationOptions>>()
-                .Get(scheme);
+            var sut = new BackOfficeAuthenticationBuilder.EnsureBackOfficeScheme<RemoteAuthenticationOptions>();
+            sut.PostConfigure(scheme, options);
 
-            Assert.AreEqual("my_cookie", authOptions.SignInScheme);
+            Assert.AreNotEqual(options.SignInScheme, Constants.Security.BackOfficeExternalAuthenticationType);
         }
     }
 }
