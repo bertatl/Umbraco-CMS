f9eb947d055fb80c8ead62497117d4e2d5dd059a
NoReply Amazon <no-reply@amazon.com>
2024-11-26 00:04:33 +0000

    Revert "[Commit] Iteration 176 #errors = 2532 <= 2529."
    This reverts commit d54353bc0dc008769e264da82bca2aec6fff466e.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
@@ -1,32 +1,19 @@
-// Copyright (c) Umbraco.
+﻿// Copyright (c) Umbraco.
 // See LICENSE for more details.
 
 using NUnit.Framework;
-using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Infrastructure.WebAssets;
 using Umbraco.Extensions;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
 {
     [TestFixture]
     public class JsInitializationTests
     {
-        private IBackOfficeJavaScriptInitializer _initializer;
-
-        [SetUp]
-        public void Setup()
-        {
-            var services = new ServiceCollection();
-            services.AddSingleton<IBackOfficeJavaScriptInitializer, BackOfficeJavaScriptInitializer>();
-            var serviceProvider = services.BuildServiceProvider();
-            _initializer = serviceProvider.GetRequiredService<IBackOfficeJavaScriptInitializer>();
-        }
-
         [Test]
         public void Parse_Main()
         {
-            var result = _initializer.Initialize("[World]", "Hello", "Blah");
+            var result = BackOfficeJavaScriptInitializer.WriteScript("[World]", "Hello", "Blah");
 
             Assert.AreEqual(
                 @"LazyLoad.js([World], function () {
