628668f08f7e8e61a87bffe560b967762546b376
NoReply Amazon <no-reply@amazon.com>
2024-11-26 04:49:55 +0000

    Revert "[Commit] Iteration 317 #errors = 2468 <= 2468."
    This reverts commit f89048e69eaf772e7d6a2cebf23c48147d64b128.

--- tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.PublishedCache.NuCache/SnapDictionaryTests.cs
@@ -100,9 +100,9 @@ public class TestHelper
             Assert.AreEqual(0, d.Count);
 
             // gen 1
-            d.Add(1, "one");
+            d.Set(1, "one");
             Assert.AreEqual(1, d.Count);
-            d.Add(1, null); // Set the value to null to effectively remove the item
+            d.Set(1, null); // Set the value to null to effectively remove the item
             Assert.AreEqual(0, d.Count); // should be gone
 
             // We can't assert on internal state, so we'll remove these assertions
