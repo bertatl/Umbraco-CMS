7be1b4efbc7c825905e46a10e0c9b667d6452a3a
NoReply Amazon <no-reply@amazon.com>
2024-11-26 20:18:18 +0000

    [Commit] Iteration 118 #errors = 2604 <= 2605.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs][CS0122][16,58] 'BackOfficeJavaScriptInitializer.WriteScript(string, string, string)' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
@@ -1,9 +1,10 @@
-﻿// Copyright (c) Umbraco.
+// Copyright (c) Umbraco.
 // See LICENSE for more details.
 
 using NUnit.Framework;
 using Umbraco.Cms.Infrastructure.WebAssets;
 using Umbraco.Extensions;
+using System.Reflection;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
 {
@@ -13,7 +14,11 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
         [Test]
         public void Parse_Main()
         {
-            var result = BackOfficeJavaScriptInitializer.WriteScript("[World]", "Hello", "Blah");
+            // Use reflection to access the WriteScript method
+            var method = typeof(BackOfficeJavaScriptInitializer).GetMethod("WriteScript", BindingFlags.NonPublic | BindingFlags.Static);
+            Assert.IsNotNull(method, "WriteScript method not found");
+
+            var result = (string)method.Invoke(null, new object[] { "[World]", "Hello", "Blah" });
 
             Assert.AreEqual(
                 @"LazyLoad.js([World], function () {
