66175bed8ad3a468a84933a557cf8b969b9e8fd4
NoReply Amazon <no-reply@amazon.com>
2024-11-26 20:15:37 +0000

    Revert "[Commit] Iteration 116 #errors = 2606 <= 2605."
    This reverts commit 0c6f6481f1314bbe1535084e561481ba8ba056b2.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Web.Common/AngularIntegration/JsInitializationTests.cs
@@ -1,10 +1,9 @@
-// Copyright (c) Umbraco.
+﻿// Copyright (c) Umbraco.
 // See LICENSE for more details.
 
 using NUnit.Framework;
 using Umbraco.Cms.Infrastructure.WebAssets;
 using Umbraco.Extensions;
-using Moq;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
 {
@@ -14,26 +13,8 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Common.AngularIntegration
         [Test]
         public void Parse_Main()
         {
-            // Arrange
-            var mockInitializer = new Mock<IBackOfficeJavaScriptInitializer>();
-            mockInitializer.Setup(m => m.WriteScript("[World]", "Hello", "Blah"))
-                .Returns(@"LazyLoad.js([World], function () {
-    //we need to set the legacy UmbClientMgr path
-    if ((typeof UmbClientMgr) !== ""undefined"") {
-        UmbClientMgr.setUmbracoPath('Hello');
-    }
+            var result = BackOfficeJavaScriptInitializer.WriteScript("[World]", "Hello", "Blah");
 
-    jQuery(document).ready(function () {
-
-        angular.bootstrap(document, ['Blah']);
-
-    });
-});");
-
-            // Act
-            var result = mockInitializer.Object.WriteScript("[World]", "Hello", "Blah");
-
-            // Assert
             Assert.AreEqual(
                 @"LazyLoad.js([World], function () {
     //we need to set the legacy UmbClientMgr path
