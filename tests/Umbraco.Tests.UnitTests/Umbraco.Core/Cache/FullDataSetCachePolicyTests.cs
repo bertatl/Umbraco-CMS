dd948f372178b0a3c14eb60ef93c0c207b2d0d80
NoReply Amazon <no-reply@amazon.com>
2024-11-26 21:29:00 +0000

    [Commit] Iteration 157 #errors = 2494 <= 2494.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs][CS0122][68,37] 'FullDataSetRepositoryCachePolicy<TEntity, TId>' is inaccessible due to its protection level``.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
@@ -12,6 +12,7 @@ using Umbraco.Cms.Core.Collections;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Infrastructure.Persistence.Repositories;
+using Umbraco.Cms.Infrastructure.Persistence.Repositories.Implement;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
 {
