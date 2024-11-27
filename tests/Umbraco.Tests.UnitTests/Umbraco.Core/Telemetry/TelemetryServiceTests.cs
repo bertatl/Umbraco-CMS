b77902fc3d885aaf767895409fd70e52cf34507b
NoReply Amazon <no-reply@amazon.com>
2024-11-27 01:50:27 +0000

    Revert "[Commit] Iteration 543 #errors = 2338 <= 2339."
    This reverts commit 5f94dcc3cf780d3c06c2455560a848cf28201c2f.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Telemetry/TelemetryServiceTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Telemetry/TelemetryServiceTests.cs
@@ -32,16 +32,13 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Telemetry
         [Test]
         public void SkipsIfCantGetOrCreateId()
         {
-            var siteIdentifierService = createSiteIdentifierService(false);
-            var telemetryServiceMock = new Mock<ITelemetryService>();
-            telemetryServiceMock.Setup(x => x.TryGetTelemetryReportData(out It.Ref<TelemetryReportData>.IsAny))
-                .Returns(false);
+            var version = CreateUmbracoVersion(9, 3, 1);
+            var sut = new TelemetryService(Mock.Of<IManifestParser>(), version, createSiteIdentifierService(false), Mock.Of<IUsageInformationService>(), Mock.Of<IMetricsConsentService>());
 
-            var result = telemetryServiceMock.Object.TryGetTelemetryReportData(out var telemetry);
+            var result = sut.TryGetTelemetryReportData(out var telemetry);
 
             Assert.IsFalse(result);
             Assert.IsNull(telemetry);
-            Mock.Get(siteIdentifierService).Verify(x => x.TryGetOrCreateSiteIdentifier(out It.Ref<Guid>.IsAny), Times.Once);
         }
 
         [Test]
