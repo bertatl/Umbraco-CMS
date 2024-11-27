93676ba0b9863515b2881b425f5b19e4a6fe3d4a
NoReply Amazon <no-reply@amazon.com>
2024-11-27 01:22:14 +0000

    Revert "[Commit] Iteration 526 #errors = 2356 <= 2356."
    This reverts commit a71f5dca04012e22bca4a80722a0f1aba49ad3dd.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/FullDataSetCachePolicyTests.cs
@@ -13,7 +13,6 @@ using Umbraco.Cms.Core.Collections;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Infrastructure.Persistence.Repositories;
-using Umbraco.Cms.Infrastructure.PublishedCache;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
 {
