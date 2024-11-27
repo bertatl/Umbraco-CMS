1402c2c4dd39670a8456b98344f81a5fa4cb1952
NoReply Amazon <no-reply@amazon.com>
2024-11-27 03:17:31 +0000

    Revert "[Commit] Iteration 719 #errors = 2272 <= 2272."
    This reverts commit e130f2a4cbc0c78f5139dc11c853e2d94d39ad3c.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Persistence/BulkDataReaderTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Persistence/BulkDataReaderTests.cs
@@ -9,7 +9,6 @@ using System.Data.Common;
 using System.Data.SqlClient;
 using NUnit.Framework;
 using Umbraco.Cms.Infrastructure.Persistence;
-using Umbraco.Cms.Infrastructure.Persistence.Dtos;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Persistence
 {
@@ -2328,7 +2327,7 @@ using (var testReader = new BulkDataReaderSubclass())
             public override bool Read() => _readCount++ < 1;
         }
 
-    private class BulkDataReaderSchemaTest : global::Umbraco.Cms.Infrastructure.Persistence.BulkDataReader
+        private class BulkDataReaderSchemaTest : BulkDataReader
         {
             /// <summary>
             /// Gets or sets a value indicating whether the column is nullable (i.e. optional).
