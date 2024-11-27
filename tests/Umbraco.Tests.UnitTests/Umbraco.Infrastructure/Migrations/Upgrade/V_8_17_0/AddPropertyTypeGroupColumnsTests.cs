1dee8268bfef791ddf2da34aa5a117c0f6093fa7
NoReply Amazon <no-reply@amazon.com>
2024-11-27 01:40:44 +0000

    Revert "[Commit] Iteration 538 #errors = 2335 <= 2335."
    This reverts commit 3f1b37b748b416709d8a39a9f164e264b3da622e.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Migrations/Upgrade/V_8_17_0/AddPropertyTypeGroupColumnsTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Migrations/Upgrade/V_8_17_0/AddPropertyTypeGroupColumnsTests.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using System.Dynamic;
-using System.Reflection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
@@ -47,8 +46,7 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Migrations.Upgrade.
                 CreatePropertyTypeGroupDto(5, "Site defaults")
             };
 
-            var populateAliasesMethod = typeof(AddPropertyTypeGroupColumns).GetMethod("PopulateAliases", BindingFlags.NonPublic | BindingFlags.Instance);
-            var populatedDtos = ((IEnumerable<object>)populateAliasesMethod.Invoke(migration, new object[] { dtos.Cast<object>() }))
+            var populatedDtos = migration.PopulateAliases(dtos.Cast<object>())
                 .OrderBy(x => ((dynamic)x).Id)
                 .ToArray();
 
